f40ad8227c0ab239fa313e6ab72f756d69393434
ElodieFon <e.fontaine.1991@hotmail.fr>
2023-03-24 09:35:10 +0100

    config firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'checkMovie';
+
+  constructor(){
+    const firebaseConfig = {
+      apiKey: "AIzaSyCzrsbm1R-ZzC8p9H4K0XUN8GSlQ4WGBpw",
+      authDomain: "checkmovies-414e0.firebaseapp.com",
+      projectId: "checkmovies-414e0",
+      storageBucket: "checkmovies-414e0.appspot.com",
+      messagingSenderId: "863103484996",
+      appId: "1:863103484996:web:7118138e8e4932efba2811",
+      measurementId: "G-GK2882C9GY"
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
 }
